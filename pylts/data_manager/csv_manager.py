ee4658227723ea61f21b63b71b892ab1214ec402
Alan Bandeira <alan.p.bandeira@gmail.com>
2018-10-11 12:52:53 -0300

    modules descriptions

---
+++ pylts/data_manager/csv_manager.py
@@ -0,0 +1,7 @@
+class CsvManager(object):
+    """docstring"""
+    
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    # funtion to processe the csv and return a matrix of data
